24eb351f7c22159497acb6b5adb342c7f237c934
asilbekali <asilbekabdugafforov7@gmail.com>
2026-03-24 18:16:28 +0500

    vercal analise added

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 import "../globals.css";
 import ResponsiveAppBar from "@/components/Navbar";
 import "../../styles/background.css";
@@ -29,12 +30,11 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <head>
-        <meta name="google-adsense-account" content="ca-pub-1116201113573585"></meta>
+        <meta name="google-adsense-account" content="ca-pub-1116201113573585" />
         <Script
           id="adsense-script"
-          async
-          strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1116201113573585"
+          strategy="beforeInteractive"
           crossOrigin="anonymous"
         />
       </head>
@@ -46,6 +46,7 @@ export default async function RootLayout({
           <main className="mt-7.5" style={{ paddingTop: "80px" }}>
             {children}
           </main>
+          <Analytics />
         </NextIntlClientProvider>
       </body>
     </html>
